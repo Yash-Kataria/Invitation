f982b96f8f7e1f47a3aab1707e3575c2b53998b4
Yash Himmatsingh Kataria <46312009+Yash-Kataria@users.noreply.github.com>
2021-12-28 12:10:06 +0530

    Wedding Card Invitation
    Aishwarya And Ninad Wedding Card Invitation

--- animation.js
+++ animation.js
@@ -9,9 +9,9 @@ $(window).on("load",function() {
         if (objectBottom < windowBottom) { //object comes into view (scrolling down)
           if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
         } 
-        else { //object goes out of view (scrolling up)
-          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-        }
+        // else { //object goes out of view (scrolling up)
+        //   if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        // }
       });
     }).scroll(); //invoke scroll-handler on page-load
   });
