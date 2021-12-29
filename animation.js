f2e2c070f12a79b7f2053abde7c4749e6b6d029b
Yash Himmatsingh Kataria <46312009+Yash-Kataria@users.noreply.github.com>
2021-12-29 18:00:49 +0530

    Wedding Invitation Card
    Aishwarya and Ninad Wedding Invitation Card

--- animation.js
+++ animation.js
@@ -1,16 +1,28 @@
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".FadeInAnimation").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight(true);
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom <= windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } 
-      // else { //object goes out of view (scrolling up)
-      //   if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-      // }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
+$(window).on("load", function () {
+  $(window)
+    .on("scroll resize", function () {
+      var window_height = $(window).height();
+      var window_top_position = $(window).scrollTop();
+      var window_bottom_position = window_top_position + window_height;
+
+      $(".FadeInAnimation").each(function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = element_top_position + element_height;
+
+        if (
+          element_bottom_position >= window_top_position &&
+          element_top_position <= window_bottom_position
+        ) {
+          if ($(this).css("opacity") == 0) {
+            $(this).fadeTo(1200, 1);
+          }
+        }
+        // else { //object goes out of view (scrolling up)
+        //   if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        // }
+      });
+    })
+    .scroll();
 });
